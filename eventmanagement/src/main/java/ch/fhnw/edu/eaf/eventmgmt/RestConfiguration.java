32ebc3a642ce13891ed4159cfeb3d64b0f7608dd
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2017-03-20 01:48:16 +0100

    Allow all CORS

---
+++ eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/RestConfiguration.java
@@ -0,0 +1,30 @@
+package ch.fhnw.edu.eaf.eventmgmt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by lukasschonbachler on 20.03.17.
+ */
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
