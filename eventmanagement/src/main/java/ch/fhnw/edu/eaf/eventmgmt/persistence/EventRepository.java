7aa843d6920f0c6a29a1c707663c22a3c165969f
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2017-03-20 15:00:22 +0100

    Cleanup

--- eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/persistence/EventRepository.java
+++ eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/persistence/EventRepository.java
@@ -1,6 +1,5 @@
 package ch.fhnw.edu.eaf.eventmgmt.persistence;
 
-//import ch.fhnw.edu.eaf.eventmgmt.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ch.fhnw.edu.eaf.eventmgmt.domain.Event;
@@ -11,33 +10,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    // Uncomment to disallow methods
-
-    //@Query("FROM File f where event = :eventId")
-    //public Iterable<File> getFilesByEventId(@Param("eventId") Event event);
-
-
-    /*
-
-    // Prevents GET /people/:id
-    @Override
-    @RestResource(exported = false)
-    public Event findOne(Long id);
-
-    // Prevents GET /people
-    @Override
-    @RestResource(exported = false)
-    public Page<Event> findAll(Pageable pageable);
-
-    // Prevents POST /people and PATCH /people/:id
-    @Override
-    @RestResource(exported = false)
-    public Event save(Event s);
-
-    // Prevents DELETE /people/:id
-    @Override
-    @RestResource(exported = false)
-    public void delete(Event t);
-
-    */
 }
--- eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/persistence/UserRepository.java
+++ eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/persistence/UserRepository.java
@@ -7,14 +7,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    /*
-    @Query("SELECT u FROM User u JOIN u.attendees a WHERE a.Id = :id")
-    public Iterable<User> getAttendeesByEventId(@Param("id") Long id);
-
-	List<Event> findByLastName(String lastName);
-
-	List<Event> findByFirstName(String firstName);
-
-	List<Event> findByEmail(String email);
-	*/
 }
