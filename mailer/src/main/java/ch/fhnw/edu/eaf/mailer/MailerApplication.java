6a8d2d3be4c3e1eb056c2df773147ff2bdba3b88
Jonas Frehner <jonas@frehner.ch>
2017-05-20 17:17:16 +0200

    Enable Mail Discovery Client so that Mailer is a Microservice that can be found

--- mailer/src/main/java/ch/fhnw/edu/eaf/mailer/MailerApplication.java
+++ mailer/src/main/java/ch/fhnw/edu/eaf/mailer/MailerApplication.java
@@ -3,9 +3,12 @@ package ch.fhnw.edu.eaf.mailer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan
 public class MailerApplication {
