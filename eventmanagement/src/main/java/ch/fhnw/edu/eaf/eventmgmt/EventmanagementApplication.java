368fb1f3c811058564f2de9d172bc85dc35d2ae3
Jonas Frehner <jonas@frehner.ch>
2017-05-22 11:47:00 +0200

    Add loadbalanced restTemplate so Eureka-Clients can be found via logical names. Add further properties to eventmanagement so the mailer is correctly accessed.

--- eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/EventmanagementApplication.java
+++ eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/EventmanagementApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -28,22 +31,13 @@ public class EventmanagementApplication {
 	public static String BASE_URL_MAILER;
 
 	public static void main(String[] args) {
-
-		try {
-			HttpURLConnection conn = (HttpURLConnection) new URL("http://localhost:8761/eureka/apps/Mailer").openConnection();
-			XPath xpath = XPathFactory.newInstance().newXPath();
-			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(conn.getInputStream());
-			BASE_URL_MAILER = xpath.evaluate("/application/instance/homePageUrl", document);
 			SpringApplication.run(EventmanagementApplication.class, args);
-		} catch (IOException e) {
-			throw new RuntimeException("Eureka Server not found: " + e.getMessage());
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (XPathExpressionException e) {
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
