6c86736c4e87415c19908ae8bc02b7594c3f9437
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2017-05-26 15:41:00 +0200

    fix(aai): attempt to fix encoding issue

--- eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/persistence/AAIRepository.java
+++ eventmanagement/src/main/java/ch/fhnw/edu/eaf/eventmgmt/persistence/AAIRepository.java
@@ -33,47 +33,42 @@ public class AAIRepository {
     public void aaiAuth(@RequestHeader(value = "surname", required = true) String surname,
                         @RequestHeader(value = "givenname", required = true) String givenname,
                         @RequestHeader(value = "mail", required = true) String mail,
-                        @RequestHeader(value = "homeorganization", required = true) String org,
-                        @RequestHeader(value = "shib-session-id", required = true) String id,
-                        @RequestHeader(value = "shib-session-index", required = true) String index,
-                        @RequestHeader(value = "shib-identity-provider", required = true) String provider,
-                        @RequestHeader(value = "shib-authentication-method", required = true) String method,
-                        @RequestHeader(value = "shib-authentication-instant", required = true) String instant,
-                        @RequestHeader(value = "shib-authncontext-class", required = true) String authncontext,
-                        @RequestHeader(value = "x-forwarded-for", required = true) String originRemoteAddr,
                         HttpServletResponse response) throws Exception {
-        //if (!originRemoteAddr.contains("https://www.cs.technik.fhnw.ch/wodss17-5-aai/")) {
-        //    throw new Exception(String.format("Unknown aai authentication provider: %s", originRemoteAddr));
-        //} else {
-            List<User> users = userRepository.findByEmail(mail);
-            if(users.size() > 0){
-                // User already exists, load profile into session
-                User user = users.get(0);
-                final CommonProfile profile = new CommonProfile();
-                profile.addAttribute(Pac4jConstants.USERNAME, user.getEmail());
-                profile.addRole("REGISTERED");
-                if(user.getRole() == 2) {
-                    profile.addRole("ADMINISTRATOR");
-                }
-                profile.addAttribute("user", user);
-                context.getSession().setAttribute("profile", profile);
-            }else{
-                // User does not exist, create him
-                final CommonProfile profile = new CommonProfile();
-                profile.addAttribute(Pac4jConstants.USERNAME, mail.trim().toUpperCase());
-                profile.addRole("REGISTERED");
-                context.getSession().setAttribute("profile", profile);
 
-                User user = new User();
-                user.setFirstName(givenname);
-                user.setLastName(surname);
-                user.setEmail(mail);
-                user.setInternal(true);
-                user.setRole(User.RoleType.REGISTERED);
-                em.persist(user);
-
+        List<User> users = userRepository.findByEmail(mail);
+        if (users.size() > 0) {
+            // User already exists, load profile into session
+            User user = users.get(0);
+            final CommonProfile profile = new CommonProfile();
+            profile.addAttribute(Pac4jConstants.USERNAME, user.getEmail());
+            profile.addRole("REGISTERED");
+            if (user.getRole() == 2) {
+                profile.addRole("ADMINISTRATOR");
             }
-        //}
+            profile.addAttribute("user", user);
+            context.getSession().setAttribute("profile", profile);
+        } else {
+            // User does not exist, create him
+            final CommonProfile profile = new CommonProfile();
+            profile.addAttribute(Pac4jConstants.USERNAME, mail.trim().toUpperCase());
+            profile.addRole("REGISTERED");
+            context.getSession().setAttribute("profile", profile);
+
+            User user = new User();
+            user.setFirstName(givenname);
+
+            String utf8String = new String(org.apache.commons.io.Charsets.ISO_8859_1.encode(surname).array());
+
+            //byte[] utf8 = new String(latin1, "ISO-8859-1").getBytes("UTF-8");
+
+            user.setLastName(utf8String);
+            user.setEmail(mail);
+            user.setInternal(true);
+            user.setRole(User.RoleType.REGISTERED);
+            em.persist(user);
+
+        }
+
         response.sendRedirect("https://www.cs.technik.fhnw.ch/wodss17-5/#/profile");
     }
 }
