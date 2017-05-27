03b40953fa7f76b9ba183a3d4783a45b3c04e081
Andreas Gassmann <andreas@andreasgassmann.ch>
2017-05-27 13:23:49 +0200

    feat(home): split upcoming and past events

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -5,20 +5,31 @@ import { ApiService } from "../../providers/api.service";
 import { Event } from '../../models/Event';
 import { RoleType } from '../../models/RoleType';
 import { DetailPage } from "../detail/detail";
-import {AuthService} from "../../providers/auth.service";
+import { AuthService } from "../../providers/auth.service";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  events: Event[];
+  upcomingEvents: Event[];
+  pastEvents: Event[];
 
   // Static binding workaround (http://stackoverflow.com/questions/39193538/how-to-bind-static-variable-of-component-in-html-in-angular-2)
   public RoleType = RoleType;
 
   constructor(private _apiService: ApiService, private navCtrl: NavController, public authService: AuthService) {
-    this._apiService.getEvents().then(data => this.events = data).catch((reason) => console.log(reason));
+    this._apiService.getEvents()
+      .then(data => {
+        let sorted = data.sort((a, b) => {
+          if (a.startTime < b.startTime) return -1;
+          if (a.startTime > b.startTime) return 1;
+          return 0;
+        })
+        this.upcomingEvents = sorted.filter(e => new Date(e.startTime) > new Date());
+        this.pastEvents = sorted.filter(e => new Date(e.startTime) < new Date());
+      })
+      .catch(console.log);
   }
 
   /**
