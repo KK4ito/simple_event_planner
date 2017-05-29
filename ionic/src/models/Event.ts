2e48510c1b8098887a698ce1ea1771da433c4241
Jonas Frehner <jonas@frehner.ch>
2017-05-29 14:38:32 +0200

    docs(Ionic): Add docs to event.ts

--- ionic/src/models/Event.ts
+++ ionic/src/models/Event.ts
@@ -1,16 +1,67 @@
 import {User} from "./User";
 export class Event {
+  /**
+   * The datetime when an event-deadline closes.
+   */
 		closingTime: string;
-		created: string;
-		description: string;
-		location: string;
-    closingMailSend:boolean;
-		endTime: string;
-		id: number;
-		name: string;
-		image: string;
-    imageUri: string;
-		startTime: string;
-		updated: string;
-		speakers: string[];
+
+  /**
+   * The date when an event was created.
+   */
+  created: string;
+
+  /**
+   * The descripition of an event.
+   */
+  description: string;
+
+  /**
+   * The location where the event will take place.
+   */
+  location: string;
+
+  /**
+   * Whether or not the automatically-to-send-mails were already sent.
+   */
+  closingMailSend:boolean;
+
+  /**
+   * The time when an event finishes.
+   */
+  endTime: string;
+
+  /**
+   * The id of an event.
+   */
+  id: number;
+
+  /**
+   * The title of an event.
+   */
+  name: string;
+
+  /**
+   * The image accompanying the event.
+   */
+  image: string;
+
+  /**
+   * The url to the image of the event.
+   */
+  imageUri: string;
+
+  /**
+   * The datetime when an event starts.
+   */
+  startTime: string;
+
+  /**
+   * The date when an event was last updated.
+   */
+  updated: string;
+
+  /**
+   * A list of all speakers that will speak at the event.
+   */
+  speakers: string[];
 }
